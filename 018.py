c38e869fd8394069e449cce4d9061dbfb0b8e29b
yurezin <vitorgithub887@gmail.com>
2023-10-31 11:32:09 -0300

    Update 018.py

--- 018.py
+++ 018.py
@@ -1,6 +1,6 @@
 
 arqv= float(input("Digite o tamanho do arquivo em mb: "))
 vlc = float(input("Digite a velocidade da conexão em mbps: "))
-tmp= arqv / vlc
+tmp= arqv /(vlc/8)
 minutos = tmp / 60
 print("{} min".format(minutos))
